c7418d2a8be4fb30db59e25a4a231a3d11a715fa
Mirvohid2412 <mirvohidmirzohidov25@gmail.com>
2026-07-24 21:42:18 +0500

    Save state before adding leave-channel history wipe feature

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -34,26 +34,7 @@ export const ApiService = {
         }
 
         // Kesh umuman bo'lmasa, kutib olamiz (faqat birinchi zapuskda)
-        // return await this._fetchAndCacheChannels();
-        return [
-            {
-                _id: "6a4bcdd31ee089d40bf06bbb",
-                telegram_id: "-1003831284898",
-                name: "1-kanal",
-                invite_link: "https://t.me/+nvS-w92jxN45YzBi",
-                is_active: true,
-                join_type: "public"
-            },
-            {
-
-                id: "6a4b6d250e00db55d1c2df6f",
-                telegram_id: "-1004375039133",
-                name: "2-kanal",
-                invite_link: "https://t.me/+qYFpw5UxQFA5OTQy",
-                is_active: true,
-                join_type: "public",
-            }
-        ]
+        return await this._fetchAndCacheChannels();
     },
 
     async _fetchAndCacheChannels() {
